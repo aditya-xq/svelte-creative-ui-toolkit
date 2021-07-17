bc351aa844b79419162279a9afeaf88e55a3bd69
aditya-xq <adityavivek1998@gmail.com>
2021-07-17 14:17:59 +0530

    added accordion

---
+++ src/accordions/Accordion.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let data: {parent: string, child: string}[];
+</script>
+
+<style src="Accordion.scss">
+</style>
+
+<div>
+    <ul>
+        {#each data as item}
+        <li>
+            <input type="checkbox" checked>
+            <i></i>
+            <h2>{item.parent}</h2>
+            <p>{item.child}</p>
+        </li>
+        {/each}
+    </ul>
+</div>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './buttons/Button.svelte';
 export { default as BlobButton } from './buttons/BlobButton.svelte';
 export { default as Link } from './links/Link.svelte';
+export { default as Accordion } from './accordions/Accordion.svelte';
