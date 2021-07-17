654ccb900dd13835ff0e7f70043d681463b18940
aditya-xq <adityavivek1998@gmail.com>
2021-07-17 16:54:02 +0530

    added toggle button

---
+++ src/buttons/ToggleButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let style: string;
+</script>
+
+<style src="Button.scss">
+</style>
+
+<div class="btn-toggle">
+    <span class={'switcher ' + ['switcher', style].filter(Boolean).join('-')}>
+        <input type="checkbox" id="switcher">
+        <label for="switcher"></label>
+    </span>
+</div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './buttons/Button.svelte';
 export { default as BlobButton } from './buttons/BlobButton.svelte';
+export { default as ToggleButton } from './buttons/ToggleButton.svelte';
 export { default as Link } from './links/Link.svelte';
 export { default as Accordion } from './accordions/Accordion.svelte';
 export { default as Input } from './formFields/Input.svelte';
