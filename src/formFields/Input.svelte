d44af0b285b91098a4e08fe3773b24f0b355d416
aditya-xq <adityavivek1998@gmail.com>
2021-07-17 15:55:36 +0530

    added Input component

---
+++ src/formFields/Input.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let label: string;
+    export let value: string;
+</script>
+
+<style src="Input.scss">
+</style>
+
+<div>
+    <label for="input-a">{label}</label>
+    <input type="text" id="input-a" bind:value={value}/>  
+</div>
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './buttons/Button.svelte';
 export { default as BlobButton } from './buttons/BlobButton.svelte';
 export { default as Link } from './links/Link.svelte';
 export { default as Accordion } from './accordions/Accordion.svelte';
+export { default as Input } from './formFields/Input.svelte';
